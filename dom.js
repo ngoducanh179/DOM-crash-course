357ee2411745b599a3f23f9a8057acc9453b119c
ngoducanh179 <46935024+ngoducanh179@users.noreply.github.com>
2019-06-26 17:45:15 +0700

    fwefwe

--- dom.js
+++ dom.js
@@ -347,31 +347,61 @@
 // container.insertBefore(div2,h1 );   
 
 
-var button = document.getElementById('button').addEventListener
-('click',buttonClick);
+//EVENTS
+
+// var button = document.getElementById('button').addEventListener('click', buttonClick);
 
 
-function buttonClick(e){
-    // console.log('button click');
-    // document.getElementById('header-title').textContent = 'changed';
-    // document.querySelector('#main').style.backgroundColor = '#f4f4f4';
-    // // console.log(e);
-    // console.log(e.target);
-    // console.log(e.target.id);
-    // console.log(e.target.className);
-    // console.log(e.target.classList);
-    // var output = document.getElementById('output');
-    // output.innerHTML = '<h3>'+e.target.id+'</h3>';
+// function buttonClick(e) {
+// console.log('button click');
+// document.getElementById('header-title').textContent = 'changed';
+// document.querySelector('#main').style.backgroundColor = '#f4f4f4';
+// // console.log(e);
+// console.log(e.target);
+// console.log(e.target.id);
+// console.log(e.target.className);
+// console.log(e.target.classList);
+// var output = document.getElementById('output');
+// output.innerHTML = '<h3>'+e.target.id+'</h3>';
 
-    // console.log(e.type);
-    // console.log(e.clientX);
-    // console.log(e.clientY);
+// console.log(e.type);
+// console.log(e.clientX);
+// console.log(e.clientY);
 
-    console.log(e.offsetX);
+// console.log(e.offsetX);
+// console.log(e.offsetY);
+
+// console.log(e.altKey);
+// console.log(e.ctrlKey);
+// console.log(e.shiftKey);
 
 
+// }
+var button = document.getElementById('button')
+var box = document.getElementById('box');
+var output = document.getElementById('output');
+// button.addEventListener('click', runEvent);
+// button.addEventListener('dblclick', runEvent);
+// button.addEventListener('mousedown', runEvent);
+// button.addEventListener('mouseup', runEvent);
+
+// box.addEventListener('mouseenter', runEvent);
+// box.addEventListener('mouseleave', fuck)
+
+// box.addEventListener('mousemove', runEvent);
+
+var iteminput = document.querySelector('input[type = "text"]');
+var form = document.querySelector('form')
+
+iteminput.addEventListener('keydown', runEvent)
+
+
+function runEvent(e) {
+    console.log('EVENT TYPE: ' + e.type);
+
+    console.log(e.target.value);
+
+    // output.innerHTML = '<h3>MouseX: ' + e.offsetX + '</h3><h3>MouseY: ' + e.offsetY + '</h3>';
+
+    // document.body.style.backgroundColor = "rgb(" + e.offsetX + "," + e.offsetY + ",40)";
 }
-
-
-
-
